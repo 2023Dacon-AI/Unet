fefb4f57be4aec7d98c4d68c8e9763c968b62bbc
Song Hyunbin <shbin0519@gmail.com>
2023-07-27 22:33:54 +0900

    pooling added

--- Hyunbin/inference_val.py
+++ Hyunbin/inference_val.py
@@ -26,6 +26,7 @@ model = smp.Unet(
     in_channels = 3,
     classes=1,
     aux_params=dict(
+        pooling='max',
         dropout=0.5,               # dropout ratio, default is None
         activation='sigmoid',      # activation function, default is None
         classes=1,                 # define number of output labels
